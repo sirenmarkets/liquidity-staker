d87727726433004712bb66fd7eeaaa5f47b80b3f
seafi <74265744+seafi@users.noreply.github.com>
2021-03-04 19:34:44 -0800

    Initial commit

---
+++ test/utils.ts
@@ -0,0 +1,11 @@
+import { BigNumber, providers, utils, Contract } from 'ethers'
+
+export const REWARDS_DURATION = 60 * 60 * 24 * 60
+
+export function expandTo18Decimals(n: number): BigNumber {
+  return BigNumber.from(n).mul(BigNumber.from(10).pow(18))
+}
+
+export async function mineBlock(provider: providers.Web3Provider, timestamp: number): Promise<void> {
+  return provider.send('evm_mine', [timestamp])
+}
